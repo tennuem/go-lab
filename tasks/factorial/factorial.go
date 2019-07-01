e0849f3d627f88e5d182031457d4a4df7fc37e8a
tennuem <tennuem@gmail.com>
2019-07-01 19:31:32 +0300

    add test for factorial

---
+++ tasks/factorial/factorial.go
@@ -0,0 +1,9 @@
+package factorial
+
+// Factorial is the product of all positive integers less than or equal to n.
+func Factorial(n int) int {
+	if n > 1 {
+		return n * Factorial(n-1)
+	}
+	return 1
+}
---
+++ tasks/factorial/factorial_test.go
@@ -0,0 +1,22 @@
+package factorial
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFactorial(t *testing.T) {
+	testData := map[int]int{
+		0: 1,
+		1: 1,
+		2: 2,
+		3: 6,
+		4: 24,
+		5: 120,
+	}
+
+	for n, v := range testData {
+		assert.Equal(t, Factorial(n), v)
+	}
+}

d1e604e50ca3a7ec77bddd200f6cceffe40f083d
tennuem <tennuem@gmail.com>
2019-07-02 13:46:58 +0300

    add test for lifo

---
+++ pkg/lifo_test.go
@@ -0,0 +1,16 @@
+package pkg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStack(t *testing.T) {
+	s := NewStack()
+
+	for i := 0; i < 10; i++ {
+		s.Push(i)
+		assert.Equal(t, s.Pop(), i)
+	}
+}

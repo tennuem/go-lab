2780ea9ff050ffb35aea55c4864ef41d5e419448
tennuem <tennuem@gmail.com>
2019-07-02 18:41:35 +0300

    change test for worker

--- pkg/worker_test.go
+++ pkg/worker_test.go
@@ -1,6 +1,8 @@
 package pkg
 
 import (
+	"sort"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,24 +10,46 @@ import (
 
 func TestWorker(t *testing.T) {
 	var (
+		wg      sync.WaitGroup
 		workers = 4
 		jobs    = 10
-		jobCh   = make(chan int, 100)
-		resCh   = make(chan int, 100)
+		jobCh   = make(chan int, 2)
+		resCh   = make(chan int, 2)
 	)
+
+	// send to worker
+	wg.Add(1)
+	go func() {
+		defer func() {
+			wg.Done()
+			close(jobCh)
+		}()
+		for j := 0; j < jobs; j++ {
+			jobCh <- j
+		}
+	}()
+
+	// get from worker
+	var result []int
+	wg.Add(1)
+	go func() {
+		defer func() {
+			wg.Done()
+			close(resCh)
+		}()
+		for j := 0; j < jobs; j++ {
+			result = append(result, <-resCh)
+		}
+	}()
 
 	for i := 0; i < workers; i++ {
 		go Worker(i, jobCh, resCh)
 	}
 
-	for j := 0; j < jobs; j++ {
-		jobCh <- j
-	}
-	close(jobCh)
+	wg.Wait()
 
+	sort.Ints(result)
 	for i := 0; i < jobs; i++ {
-		res := <-resCh
-
-		assert.Equal(t, res/2, i)
+		assert.Equal(t, result[i]/2, i)
 	}
 }

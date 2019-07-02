d3a1ab0a06213237bf4ebca4f6cc073392fe4a1b
tennuem <tennuem@gmail.com>
2019-07-02 17:07:01 +0300

    change insertion sort

--- pkg/algorithms/sort/insertion.go
+++ pkg/algorithms/sort/insertion.go
@@ -1,42 +1,22 @@
 package sort
 
-import (
-	"time"
-)
-
-func NewInsertion() Sort {
-	return &insertionSort{}
-}
-
-type insertionSort struct {
-	Operations int
-	Durations  time.Duration
-}
-
-func (s *insertionSort) Result(array []int) []int {
-
-	// Time of algorithm execution
-	start := time.Now()
-	defer func(start time.Time) {
-		s.Durations = time.Since(start)
-	}(start)
-
-	for i := 1; i < len(array); i++ {
-		for j := i; j > 0 && array[j] < array[j-1]; j-- {
-
-			// Counting number operations
-			s.Operations++
-
-			array[j-1], array[j] = array[j], array[j-1]
+func InsertionSort1(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		for j := i; j > 0 && arr[j] < arr[j-1]; j-- {
+			arr[j-1], arr[j] = arr[j], arr[j-1]
 		}
 	}
-	return array
+	return arr
 }
 
-func (s *insertionSort) Operation() int {
-	return s.Operations
-}
-
-func (s *insertionSort) Duration() time.Duration {
-	return s.Durations
+func InsertionSort2(arr []int) []int {
+	var i, j int
+	for i = 1; i < len(arr); i++ {
+		for j = 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
 }
--- pkg/algorithms/sort/insertion_test.go
+++ pkg/algorithms/sort/insertion_test.go
@@ -6,15 +6,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInsertionSort(t *testing.T) {
-	var (
-		testData = []int{5, 4, 2, 3, 1, 9, 7, 8, 0, 6}
-		sort     = NewInsertion()
-	)
+func TestInsertionSort1(t *testing.T) {
+	testData := []int{5, 4, 2, 3, 1, 9, 7, 8, 0, 6}
 
-	res := sort.Result(testData)
+	res := InsertionSort1(testData)
 
 	for i := 0; i < len(testData); i++ {
 		assert.Equal(t, res[i], i)
+	}
+}
+
+func TestInsertionSort2(t *testing.T) {
+	testData := []int{5, 4, 2, 3, 1, 9, 7, 8, 0, 6}
+
+	res := InsertionSort2(testData)
+
+	for i := 0; i < len(testData); i++ {
+		assert.Equal(t, res[i], i)
+	}
+}
+
+func BenchmarkInsertionSort1(b *testing.B) {
+	testData := []int{5, 4, 2, 3, 1, 9, 7, 8, 0, 6}
+
+	for i := 0; i < b.N; i++ {
+		InsertionSort1(testData)
+	}
+}
+
+func BenchmarkInsertionSort2(b *testing.B) {
+	testData := []int{5, 4, 2, 3, 1, 9, 7, 8, 0, 6}
+
+	for i := 0; i < b.N; i++ {
+		InsertionSort2(testData)
 	}
 }
